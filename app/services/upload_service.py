0ef6538f242345e0879be1c1763b2c9b0d474839
Shreyash Lange <Shreyashlange2006@gmail.com>
2026-07-22 16:11:31 +0530

    Fix uploads folder creation

--- app/services/upload_service.py
+++ app/services/upload_service.py
@@ -1,17 +1,17 @@
 import os
-
 from flask import current_app
 from werkzeug.utils import secure_filename
 
 
 def save_uploaded_file(file):
+    upload_folder = current_app.config["UPLOAD_FOLDER"]
+
+    # Create uploads folder if it doesn't exist
+    os.makedirs(upload_folder, exist_ok=True)
 
     filename = secure_filename(file.filename)
 
-    filepath = os.path.join(
-        current_app.config["UPLOAD_FOLDER"],
-        filename
-    )
+    filepath = os.path.join(upload_folder, filename)
 
     file.save(filepath)
 
